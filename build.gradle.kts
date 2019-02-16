b84e42c38c945672c35b72fc40fb16aea1471fb2
BirjuVachhani <brvachhani@gmail.com>
2019-02-16 16:25:44 +0530

    initial commit
    Signed-off-by: BirjuVachhani <brvachhani@gmail.com>

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+buildscript {
+    repositories {
+        //        maven { url = URI("https://jitpack.io") }
+        google()
+        jcenter()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:3.3.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.21")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+//        maven { url = URI("https://jitpack.io") }
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
+
